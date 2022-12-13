2dea433ef8629880c05da8a9c7ed2600a477a1a6
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2022-12-13 20:05:26 -0300

    Update DML - delete e update.sql

--- Scripts/DML - delete e update.sql
+++ Scripts/DML - delete e update.sql
@@ -1,32 +1,44 @@
-DELETE FROM usuario; 
+DELETE FROM alugar WHERE usuario_email IN (SELECT email
+	FROM usuario
+		 WHERE email LIKE 'a%' OR email LIKE 'A%' ); 
+            
+DELETE FROM usuario WHERE email LIKE 'a%' OR email LIKE 'A%';
 
-DELETE FROM usuario WHERE tipo_user = ‘visitante’;
-DELETE FROM usuario WHERE email = 'jessica13@gmail.com';
+DELETE FROM usuario WHERE email IN (SELECT email WHERE telefone IS NULL);
 
-DELETE FROM usuario WHERE matricula = ‘0020011194’;
+DELETE FROM usuario WHERE email IN (SELECT email WHERE telefone = '9999999999');
 
-DELETE FROM usuario WHERE nome = ‘%aiara’;
+DELETE FROM alugar WHERE usuario_email IN (SELECT email
+	FROM usuario
+		 WHERE tipo_user = 'visitante');
+            
+DELETE FROM usuario WHERE email in (SELECT email WHERE tipo_user = 'visitante');
 
-UPDATE usuario
-SET nome = 'Vitoria Oliveira'
-WHERE matricula = '00200011897';
+DELETE FROM alugar WHERE usuario_email IN (SELECT email
+	FROM usuario
+		 WHERE email LIKE '%@gmail.com' );
+         
+DELETE FROM usuario WHERE email LIKE '%@gmail.com';
+
+
 
 UPDATE usuario
 SET nome = 'Vitoria Oliveira'
 WHERE email = 'vitoria@gmail.com';
 
 UPDATE usuario
-SET telefone = '11 99999-9999'
-WHERE matricula = '0020011193'; 
+SET telefone = '99999999'
+WHERE email = 'gabriel@gmail.com'; 
 
 UPDATE usuario
 SET tipo_user = 'Aluno'
 WHERE email = 'carolfac@gmail.com';
 
 UPDATE usuario
-SET email = 'gaboramalho@gmail.com'
-WHERE nome = 'Gabriel'; 
+SET nome = 'ayrton maia'
+WHERE email = 'ayrton@gmail.com'; 
 
 UPDATE usuario
 SET tipo_user = 'aluno'
-WHERE nome LIKE 'richar%';  
+WHERE email = 'kaykay@gmail.com';
+
